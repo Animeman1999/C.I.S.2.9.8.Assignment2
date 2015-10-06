ea1e56a875e42a6356e4297d0fbfdc9ca0b059fc
Jeffrey Martin <jmartin5229@mail.kvcc.edu>
2015-10-06 16:00:26 -0400

    Defined buttons and Added First Data Validation

--- app/src/main/java/edu/kvcc/cis298/cis298assignment2/TemperatureConverter.java
+++ app/src/main/java/edu/kvcc/cis298/cis298assignment2/TemperatureConverter.java
@@ -4,13 +4,67 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class TemperatureConverter extends AppCompatActivity {
+
+    private Button mConvertButton;
+
+    private RadioButton mFromChoice1;  // RADIO BUTTONS IN THE FROM GROUP
+    private RadioButton mFromChoice2;
+    private RadioButton mFromChoice3;
+    private RadioButton mFromChoice4;
+
+    private RadioButton mToChoice5;  // RADIO BUTTONS IN THE TO GROUP
+    private RadioButton mToChoice6;
+    private RadioButton mToChoice7;
+    private RadioButton mToChoice8;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temperature_converter);
+
+
+
+
+        mFromChoice1 = (RadioButton) findViewById(R.id.radioButton);
+        mFromChoice2 = (RadioButton) findViewById(R.id.radioButton2);
+        mFromChoice3 = (RadioButton) findViewById(R.id.radioButton3);
+        mFromChoice4 = (RadioButton) findViewById(R.id.radioButton4);
+
+        mToChoice5 = (RadioButton) findViewById(R.id.radioButton5);
+        mToChoice6 = (RadioButton) findViewById(R.id.radioButton6);
+        mToChoice6 = (RadioButton) findViewById(R.id.radioButton7);
+        mToChoice7 = (RadioButton) findViewById(R.id.radioButton8);
+
+        mConvertButton = (Button) findViewById(R.id.convertButton);
+        mConvertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String tempatureString = ((EditText) findViewById(R.id.editText)).getText().toString();
+
+
+                if (tempatureString != null && !tempatureString.isEmpty()){// MAKE SURE THAT A TEMPERATURE HAS BEEN ENTERED
+
+                }
+                else {
+                    //                                                          ERROR MESSAGE WHEN TEMPERATURE HAS NOT BEEN ENTERED
+                    Toast.makeText(TemperatureConverter.this,R.string.no_tempature_error_toast, Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
     }
 
     @Override
